6ddebc6350919300be4ca81396bc721ba8056503
Kim Jin Woo <98931388+gugul42@users.noreply.github.com>
2024-02-23 13:44:21 +0900

    Add files via upload

---
+++ chabot_execute.py
@@ -0,0 +1,170 @@
+####################################################################
+import os
+import json
+import torch
+import pickle
+import numpy as np
+import pandas as pd
+import urllib.request
+from tqdm import tqdm
+from openai import OpenAI
+from google.cloud import texttospeech
+from customer_chatbot import *
+
+import tiktoken
+from langchain import OpenAI, ConversationChain
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+
+import warnings
+warnings.filterwarnings('ignore')
+####################################################################
+
+
+# DB Load
+vector_dimension = 768  # 벡터 차원
+annoy_index = AnnoyIndex(vector_dimension, 'angular')
+annoy_key   = AnnoyIndex(vector_dimension, 'angular')
+annoy_index.load('./vector_db/faq_vector_db.ann')  # 저장된 인덱스 파일 로드
+annoy_key.load('./vector_db/key_vector_db.ann')  # 저장된 인덱스 파일 로드
+
+#탬플릿 데이터 로드
+template = pd.read_excel('./data/chatbot_template.xlsx')
+
+# 템플릿 Dict 형태로 변환
+metadata = []
+for _, row in template.iterrows():
+    # 질문, 답변, 검색베이스(키워드)로 나눠 저장
+    temp = {'question':row['질문 형태'],
+            'answer':row['답변'] ,
+            'keyword':row['검색베이스']
+            }
+    metadata.append(temp)
+
+# LLM Load
+api_key = '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' # Open API 
+client = OpenAI(api_key = api_key)
+llm = ChatOpenAI(model_name = 'gpt-3.5-turbo', openai_api_key=api_key, temperature=0)
+
+
+# the conversation chain
+memory = ConversationBufferWindowMemory(llm=llm, k=2)
+conversation = ConversationChain(llm=llm,
+                                 memory=memory,
+                                 verbose=False)
+
+def execute_chatbot(query):
+    # 대화 종료 구문
+    if query == '':
+        result = '대화가 종료됩니다.'
+        print('대화가 종료됩니다.')
+    else:
+        # 유사 질문 탐색
+        top_num, result_idx = find_answer_text(query, annoy_index, annoy_key, metadata)
+
+        # Generated Knowledge Prompting : 6번
+        my_template = (
+            "#당신은 크레딧커넥트의 고객 상담원입니다. 높임말을 사용하여 답변하세요.#\n"
+            "Question : {x1}\n"
+            "Knowledge : {answer1}\n"
+            "Question : {x2}\n"
+            "Knowledge : {answer2}\n"
+            "Question : {x3}\n"
+            "Knowledge : {answer3}\n"
+            "Question : {query}\n"
+            "Answer : "
+        )
+        prompt = PromptTemplate.from_template(my_template)
+
+        # 유사한 질문이 없는 경우 
+        if min(top_num) > 0.3:
+            result = '고객님 죄송합니다. 해당 문제를 보다 잘 해결하기 위해서는 상세한 설명이 필요합니다.\n자세한 설명을 작성해주시거나 혹은 고객센터(02-761-0171 또는 02-761-0172)로 연락주세요.\n'
+            print(result)
+        else:
+            # 답변 생성 
+            prompt_result = prompt.format(query=query,
+                            x1=metadata[result_idx[0]]['question'],answer1=metadata[result_idx[0]]['answer'],
+                            x2=metadata[result_idx[1]]['question'],answer2=metadata[result_idx[1]]['answer'],
+                            x3=metadata[result_idx[2]]['question'],answer3=metadata[result_idx[2]]['answer'])
+
+            result = conversation.predict(input = prompt_result)
+            print('질문 : ',query)
+            print('답변 : ',result)
+            print('\n')
+    return result
+            
+
+def execute_callbot(client):
+    ## 1. Recoding
+    WAVE_OUTPUT_FILENAME = "./audio/recoding.wav" # 녹음 저장할 위치
+    recoding(WAVE_OUTPUT_FILENAME)
+
+    # 2. Speech to Text
+    recoding_data = speech_to_text_whisper(client, WAVE_OUTPUT_FILENAME)
+    
+    if ('금리' in recoding_data)|('한도' in recoding_data)|('대출' in recoding_data):
+        gen_result = execute_loanbot(query)
+    else:
+        # 3. LLM
+        gen_result = execute_chatbot(recoding_data)
+
+    # 4. Text to Speech
+    AUDIO_OUTPUT = "./audio/result.mp3"
+    api_path = "../ccm-customer-46e2457b7dfa.json"# Google TTS KEY
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=api_path
+    client = texttospeech.TextToSpeechClient()
+    text_to_speech_google(client, gen_result, AUDIO_OUTPUT)
+    IPython.display.Audio(AUDIO_OUTPUT)
+        
+        
+def execute_loanbot(query):
+    # 대화 종료 구문
+    if query == '':
+        result = '대화가 종료됩니다.'
+        print('대화가 종료됩니다.')
+    else:
+        interest = pd.read_csv('./data/interest.csv')
+        limit = pd.read_csv('./data/limit_by.csv')
+        customer_info = pd.read_csv('./data/seller_infomation.csv')
+
+        # 데이터 변환
+        customer_info = customer_info.sum().reset_index()
+        customer_info['ratio'] = customer_info[0]/customer_info[0].sum()
+
+        # 뱅크별 금리 한도 조합
+        banks = ['키움','도이치','KB']
+        banks_dict = {}
+        for b in banks:
+            b_limit = 0
+            b_inter = 0
+            for i in range(len(limit[b])):
+                b_limit += float(limit[b][i][:-1])/100*customer_info[0][i]
+                b_inter += float(interest[b][i][:-1])/100*customer_info['ratio'][i]
+            banks_dict[b]= [b_limit,b_inter]
+
+
+        # Generated Knowledge Prompting
+        my_template = (
+            "#당신은 크레딧커넥트의 고객 상담원입니다. 높임말을 사용하여 답변하세요.#\n"
+            "다음은 대출 정보입니다.\n"
+            "{bank1} 은행의 한도는 {limit1}만원, {bank2} 은행의 한도는 =  {limit2}만원, {bank3} 은행의 한도는 = {limit3}만원\n"
+            "{bank1} 은행의 금리는 {inter1:.2f}%, {bank2} 은행의 금리는 =  {inter2:.2f}%, {bank3} 은행의 금리는 = {inter3:.2f}%\n"
+            "고객의 질문입니다 : {query}"
+            "Answer : "
+        )
+        prompt = PromptTemplate.from_template(my_template)
+
+        # 답변 생성 
+        banks = list(banks_dict.keys())
+        prompt_result = prompt.format(query=query,
+                        bank1=banks[0],limit1=banks_dict[banks[0]][0],inter1=banks_dict[banks[0]][1]*100,
+                        bank2=banks[1],limit2=banks_dict[banks[1]][0],inter2=banks_dict[banks[1]][1]*100,
+                        bank3=banks[2],limit3=banks_dict[banks[2]][0],inter3=banks_dict[banks[2]][1]*100)
+
+        result = conversation.predict(input = prompt_result)
+        print('질문 : ',query)
+        print('답변 : ',result)
+        print('\n')
+    return result
---
+++ customer_chatbot.py
@@ -0,0 +1,273 @@
+#필요한 라이브러리#####################################################
+
+import wave
+import torch
+import pickle 
+import pyaudio
+import IPython
+import tiktoken
+import numpy as np
+from openai import OpenAI
+from annoy import AnnoyIndex
+from google.cloud import texttospeech
+from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+from langchain.chains import LLMChain
+from langchain import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
+
+import warnings
+warnings.filterwarnings('ignore')
+################################################################
+
+#.0. 필요 모델 호출
+## SBERT Model
+model_dir = 'snunlp/KR-SBERT-V40K-klueNLI-augSTS'
+model_sbert = SentenceTransformer(model_dir, device='cpu')
+
+## Reranker Model
+tokenizer = AutoTokenizer.from_pretrained("Dongjin-kr/ko-reranker")
+model = AutoModelForSequenceClassification.from_pretrained("Dongjin-kr/ko-reranker")
+model.eval()
+
+################################################################
+
+
+def make_vector_db(metadata, model_sbert):
+    """
+    Annoy와 SBERT 모델을 통해 Vector DB를 만드는 함수
+    metadata    : Q, A, keyword Dict
+    model_sbert : BERT Model
+    """
+    # 1. Annoy 인덱스 초기화 (벡터 차원과 거리 측정 방법 설정)
+    vector_dimension = 768  # BERT 벡터 차원
+    annoy_quest = AnnoyIndex(vector_dimension, 'angular')
+    annoy_key   = AnnoyIndex(vector_dimension, 'angular')
+
+    # 2. Annoy Index 생성
+    idx = 0
+    for i in range(len(metadata)):
+        question = metadata[i]['question']
+        keyword  = metadata[i]['keyword']
+
+        # 벡터 생성 및 Annoy 인덱스에 추가
+        question_vector = model_sbert.encode(question)
+        key_vector      = model_sbert.encode(keyword)
+        annoy_quest.add_item(idx, question_vector)
+        annoy_key.add_item(idx, key_vector)
+        idx += 1
+
+    # Annoy 인덱스 구축
+    annoy_quest.build(10)  # 10개의 트리로 구축
+    annoy_key.build(10)
+
+    # DB 저장
+    annoy_quest.save('./vector_db/faq_vector_db.ann') # 경로 수정 필요
+    annoy_key.save('./vector_db/key_vector_db.ann')   # 경로 수정 필요
+    print('DB Complete')
+
+
+def recoding(WAVE_OUTPUT_FILENAME):
+    """
+    pyaudio 라이브러리를 이용하여 마이클를 통해 목소리 녹음
+    WAVE_OUTPUT_FILENAME : Save Audio File Path
+    """
+    # 0. 녹음 설정
+    FORMAT = pyaudio.paInt16  # 오디오 포맷
+    CHANNELS = 1              # 모노 채널
+    RATE = 44100              # 샘플링 레이트(44100Hz)
+    CHUNK = 1024              # 프레임당 버퍼의 크기
+    RECORD_SECONDS = 5        # 녹음할 시간(초)
+
+    # 1. PyAudio 생성
+    audio = pyaudio.PyAudio()
+
+    # 2. 녹음 시작
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
+    print("녹음 중...")
+
+    # 3. 지정된 시간 동안 오디오 데이터를 버퍼에 저장
+    frames = []
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    # 4. 녹음 종료
+    print("녹음 완료.")
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # 5. WAV 파일로 저장
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(audio.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+
+def speech_to_text_whisper(client, WAVE_OUTPUT_FILENAME):
+    """
+    OpenAI STT : Whisper 활용
+    STT 모델을 활용하여 오디오 파일을 텍스트로 변환
+    client               : OPEN AI Client
+    WAVE_OUTPUT_FILENAME : Path Audio File
+    """
+    # 1. 오디오 파일을 텍스트로 변환
+    audio_file= open(WAVE_OUTPUT_FILENAME, "rb")
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1",      # 모델명
+        file=audio_file,        # 변환할 파일
+        response_format="text"  # 변환 결과물 형태
+    )
+    print('변환된 텍스트 : ',transcript)
+    return transcript
+
+
+def create_chat(api_key):
+    """
+    GPT Client 생성 함수
+    Clinet 기반 이전 K개까지 질문 같이 전송하는 ConversationChain 생성
+    api_key : Open AI API Key
+    """
+    # 1. LLM 모델 생성
+    llm = ChatOpenAI(model_name = 'gpt-3.5-turbo', openai_api_key=api_key, temperature=0) # temperature는 얼마나 신뢰도 있는 답변을 생성할지 설정(0은 최대한 보수적으로 답변)
+
+    # 2. conversation chain 생성
+    memory = ConversationBufferWindowMemory(llm=llm, k=2) # k는 이전 대화를 몇개까지 기억할지 설정값
+    conversation = ConversationChain(llm=llm,
+                                     memory=memory,
+                                     verbose=False)
+    return conversation
+
+
+
+def chat_generate(query, conversation,
+                  model_sbert, annoy_index, annoy_key, metadata):
+    """
+    고객 질문에 대한 LLM 답변 생성
+    query        : New Question
+    conversation : ConversationChain of LangChain(GPT)
+    model_sbert  : BERT Model
+    annoy_index  : Annoy of Question DB
+    annoy_key    : Annoy of Keyword DB
+    metadata     : Dict of Q, A, Keyword
+    """
+    # 0. 대화 종료 구문
+    if query == '':
+        print('대화가 종료됩니다.')
+    else:
+        # 1. 유사 질문 탐색(RAG)
+        score, answer, keyword, question = find_answer_text(query, model_sbert,
+                                                       annoy_index, annoy_key, metadata)
+    # 2. Generated Knowledge Prompting
+    my_template = (
+        "#당신은 크레딧커넥트의 고객 상담원입니다. 높임말을 사용하여 2~3문장으로 답변하세요.#\n"
+        "Question : {x}\n"
+        "Knowledge : {answer}\n"
+        "Answer : "
+    )
+    prompt = PromptTemplate.from_template(my_template)
+
+    # 3. 답변 생성
+    result = conversation.predict(input = prompt.format(x=query,answer=answer))
+    print('답변 : ',result)
+    return result
+
+
+def get_top_n(list_a, num):
+    '''
+    리스트 변수에서 상위 num개를 추출하는 코드
+    list_a : [int, int, ...] 
+    num    : int, 추출하고 싶은 개수 
+    '''
+    tmp = list_a.copy()
+    tmp.sort()
+   
+    top_num = tmp[-num:]    # 뒤에서부터 추출
+    top_idx = [list_a.index(x) for x in top_num]
+    return top_num, top_idx
+
+def exp_normalize(x):
+    """
+    유사도 점수 변환하는 코드
+    x : score
+    """
+    b = x.max()
+    y = np.exp(x - b)
+    return y / y.sum()
+
+def find_answer_text(new_question, annoy_index, annoy_key, metadata, k=20):
+    """
+    new_question : New Question
+    annoy_index  : Annoy of Questions
+    annoy_key    : Annoy of Keywords
+    metadata     : Dict of Q, A, Keyword
+    k            : 벡터 DB에서 추출할 후보 개수
+    """
+    # 1) 신규변수 Embedding
+    new_question_vector = model_sbert.encode(new_question, batch_size=512)
+
+    # 2) Vector 기반 유사 질문 추출
+    question_result = annoy_index.get_nns_by_vector(new_question_vector, k, include_distances=True)
+    key_result = annoy_key.get_nns_by_vector(new_question_vector, k, include_distances=True)
+
+    # 3) 키워드 유사도와 질문 유사도 교차 검증
+    and_idx   = list(set(question_result[0])&set(key_result[0]))
+
+    # 4) 최종 유사도 후보 변환(신규 질문 : 유사 질문)
+    quest_search = [[new_question, metadata[i]['question']] for i in and_idx]
+    key_search = [[new_question, metadata[i]['keyword']] for i in and_idx]
+    # 5) Reranker 유사도 산출
+    input_q = tokenizer(quest_search, padding=True, truncation=True, return_tensors='pt', max_length=512)
+    input_k = tokenizer(key_search, padding=True, truncation=True, return_tensors='pt', max_length=512)
+    scores_q = model(**input_q, return_dict=True).logits.view(-1, ).float()
+    scores_k = model(**input_k, return_dict=True).logits.view(-1, ).float()
+    # 6) 점수 변환
+    scores_q = exp_normalize(scores_q.detach().numpy())
+    scores_k = exp_normalize(scores_k.detach().numpy())
+    # 7) 점수 통합 및 상위 질문 추출
+    scores = scores_q + scores_k
+    top_num_q, top_idx_q = get_top_n(list(scores), 3) # 유사 질문 3개 추출
+    result_idx = [and_idx[i] for i in top_idx_q]
+    return top_num_q, result_idx
+
+
+def text_to_speech_google(client, gen_result, AUDIO_OUTPUT):
+    """
+    Text to Speech DEF
+    client       : Google Cloud Client
+    gen_result   : LLM Result 
+    AUDIO_OUTPUT : Save File Path
+    """
+    # 1. Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=gen_result)
+
+    # 2. Build the voice request, select the language code ("en-US") and the ssml
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="ko-KR",
+        name="ko-KR-Standard-B",
+        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+    )
+
+    # 3. Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # 4. Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # 5. The response's audio_content is binary.
+    with open(AUDIO_OUTPUT, "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
